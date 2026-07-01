2ffc7596aac3eb0cbc51ab83be762517d8910cf7
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2026-07-01 00:38:14 -0300

    alterando  metodo de conexao com db

--- src/db/db-connector.ts
+++ src/db/db-connector.ts
@@ -1,6 +1,6 @@
-import { env } from '#/config/env';
+import { env } from '../config/env';
 import { Sequelize } from 'sequelize-typescript';
-import databaseConfig from "#/config/database"
+import databaseConfig from "../config/database"
 
 export default function dbConnection() {
   const config = {
@@ -8,16 +8,17 @@ export default function dbConnection() {
       dialect: databaseConfig.development.dialect as "sqlite",
       storage: databaseConfig.development.storage,
       loggin: databaseConfig.development.loggin,
+      repositoryMode: databaseConfig.development.repositoryMode,
       models: [databaseConfig.development.models],
-      repositoryMode: true,
     },
     production: {
       dialect: databaseConfig.production.models as "sqlite",
       storage: databaseConfig.production.storage,
       loggin: databaseConfig.production.models,
+      repositoryMode: databaseConfig.development.repositoryMode,
       models: [databaseConfig.production.models],
-      repositoryMode: true,
     }
   }
+
   return new Sequelize(config[env.NODE_ENV]);
 }
