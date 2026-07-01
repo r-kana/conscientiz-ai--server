0d88a9381901231a8ea94ba4d4a86c97cd5fc450
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2026-07-01 00:37:40 -0300

    corrigindo configrucaoes de banco de dado

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,16 @@
 export default {
   development: {
     dialect: "sqlite",
-    storage: "/src/db/development.sqlite",
+    storage: process.env.INIT_CWD + "/src/db/development.sqlite",
     loggin: false,
-    models: process.env.INIT_CWD + "src/models",
+    models: process.env.INIT_CWD + "/src/app/models",
+    repositoryMode: true,
   },
   production: {
     dialect: "sqlite",
     storage: process.env.INIT_CWD + "/src/db/production.sqlite",
     loggin: false,
-    models: process.env.INIT_CWD + "src/models",
+    models: process.env.INIT_CWD + "/src/app/models",
+    repositoryMode: true,
   }
 }
