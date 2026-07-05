dca5396a20351fce4979952b9116dff58ff27f9d
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2026-07-04 22:38:17 -0300

    corrigindo env

--- src/config/env/env.d.ts
+++ src/config/env/env.d.ts
@@ -1,12 +1,9 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      // Environment
       NODE_ENV: 'development' | 'production',
-      // Database
-      DATABASE_PATH: string,
-      // App
       APP_NAME: string,
+      APP_URL: string,
       APP_PORT: number,
       JWT_SECRET: string,
       FRONTEND_URL: string,
