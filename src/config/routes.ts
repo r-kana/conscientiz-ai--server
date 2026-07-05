801f9a84c1545a5b153056f26508fc9bd8621a78
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2026-07-04 22:37:50 -0300

    corrigindo rotas

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -4,15 +4,17 @@ import CommentsController from "../app/controllers/comments-controller";
 import { QuestionsController } from "../app/controllers/questions-controller";
 
 export const routes = async (app: FastifyInstance) => {
-  app.get('/responses', (request: FastifyRequest, reply: FastifyReply) => { reply.send({ hello: 'world' })})
+  app.get('/responses', (request: FastifyRequest, reply: FastifyReply) => { reply.send({ hello: 'world' }) })
 
-  app.get("/posts", await PostsController.index)
-  app.get("/posts/:id", await PostsController.show)
+  app.get("/posts", PostsController.index)
+  app.get("/posts/:id", PostsController.show)
+  app.post("/posts/:id/rating", PostsController.rating)
 
-  app.post("/comments", await CommentsController.create)
-  app.get("/posts/:id/comments", await CommentsController.index)
+  app.post("/comments/:id", CommentsController.create)
 
-  app.post("/questions", await QuestionsController.create)
-  app.get("/questions", await QuestionsController.index)
+  app.post("/questions", QuestionsController.create)
+  app.get("/questions", QuestionsController.index)
+  app.get("/questions/:id", QuestionsController.show)
+  app.get("/questions/search", QuestionsController.search)
 
 }
