611111be43a32964bd8770fa514ba9f1454e3e59
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2026-07-04 22:37:01 -0300

    corrigindo params

--- src/app/controllers/comments-controller.ts
+++ src/app/controllers/comments-controller.ts
@@ -1,28 +1,27 @@
 import { ApplicationController } from "./application-controller";
 import type { FastifyRequest, FastifyReply } from "fastify";
-import type { ICreateCommentParamsSchema } from "../schemas/comment/create-comment-params-schema";
+import type { ICreateCommentParamsSchema } from "../schemas/comment-params-schema";
 import Comment from "../models/comment";
 
-export default class CommentsController extends ApplicationController
-{
-  static async create (request: FastifyRequest, reply: FastifyReply){
-    const params = request.params as ICreateCommentParamsSchema
+export default class CommentsController extends ApplicationController {
+  static async create(request: FastifyRequest, reply: FastifyReply) {
+    const params = request.body as ICreateCommentParamsSchema
     const controller = new CommentsController();
     const commentRepository = controller.getRepository(Comment);
-    
-    await commentRepository.create({...params});
+
+    await commentRepository.create({... params});
     controller.closeConnection();
 
-    return reply.status(200).send({message: "Comment created"})
+    return reply.status(200).send({ message: "Comment created" })
   }
 
-  static async index (request: FastifyRequest, reply: FastifyReply){
+  static async index(request: FastifyRequest, reply: FastifyReply) {
     const params = request.params as { id: number }
     const controller = new CommentsController();
     const commentRepository = controller.getRepository(Comment);
-    const comments = await commentRepository.findAll({ where: {id: params.id},  order: [['createdAt', 'DESC']] })
+    const comments = await commentRepository.findAll({ where: { id: params.id }, order: [['createdAt', 'DESC']] })
     controller.closeConnection();
-    
-    return reply.status(200).send({...comments})
+
+    return reply.status(200).send({ ...comments })
   }
 }
